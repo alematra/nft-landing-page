125dafb65b3e61c2cc6bbf2d1e26f16b4eec7b59
matra <ale.matri@gmail.com>
2022-02-26 10:29:29 +0100

    reveal date faq

--- js/countdown.js
+++ js/countdown.js
@@ -1,6 +1,13 @@
 //Countdown Timer
 const clockdiv = document.getElementById("countdown");
-const countDownTime = new Date("Mar 06, 2022 4:00:00 PM UTC").getTime();
+const revealDateSpan = document.getElementById("revealDate");
+const revealDate = new Date("Mar 20, 2022 8:00:00 PM UTC");
+y = revealDate.getFullYear();
+m = revealDate.getMonth() + 1;
+d = revealDate.getDate();
+h = revealDate.getHours();
+const countDownTime = revealDate.getTime();
+
 
 const countdownfunction = setInterval(function () {
   const now = new Date().getTime();
@@ -13,10 +20,12 @@ const countdownfunction = setInterval(function () {
   if (diff < 0) {
     clockdiv.style.display = "none";
     clearInterval(countdownfunction);
+    revealDate.innerHTML = "passed";
   } else {
     clockdiv.querySelector(".days").innerHTML = days;
     clockdiv.querySelector(".hours").innerHTML = hours;
     clockdiv.querySelector(".minutes").innerHTML = minutes;
     clockdiv.querySelector(".seconds").innerHTML = seconds;
+    revealDateSpan.innerHTML = m + "/" + d + "/" + y + " at " + h + ":00";
   }
 });
